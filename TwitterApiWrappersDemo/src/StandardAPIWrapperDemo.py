2d245462039c8b368d56243fba278927c482d4bd
BareBeaverBat <ssiliss72@gmail.com>
2020-02-14 20:15:07 -0500

    add credential management helper and implement working POC for getting twitter user data

--- TwitterApiWrappersDemo/src/StandardAPIWrapperDemo.py
+++ TwitterApiWrappersDemo/src/StandardAPIWrapperDemo.py
@@ -1,2 +1,28 @@
+import tweepy
+
+import src.TwitterCredentialManagement as tcm
+
+TEST_TWITTER_USERNAME = "ScottSSalisbur1"
+
 if __name__ == '__main__':
     print("running tweepy demo program")
+
+    apiKey, apiSecretKey, accessToken, accessTokenSecret = tcm.fetchAPICredentials()
+
+    auth = tweepy.OAuthHandler(apiKey, apiSecretKey)
+    auth.set_access_token(accessToken, accessTokenSecret)
+
+    twitterStandardApi = tweepy.API(auth)
+
+    try:
+        twitterStandardApi.verify_credentials()
+        print("Twitter credentials valid")
+    except:
+        print("Tweepy unable to authenticate with Twitter")
+
+    user = twitterStandardApi.get_user(TEST_TWITTER_USERNAME)
+    print("Test User Details:")
+    print("Account creation date= " + str(user.created_at))
+    print("ID String= " + user.id_str)
+
+    # todo add toy trend fetch
